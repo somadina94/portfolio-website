3f778b7c452c6910508876f184d68dd95f331616
somadina94 <williamsonuaguluchi94@gmail.com>
2025-07-28 13:52:55 +0100

    header fix

--- components/general/header.tsx
+++ components/general/header.tsx
@@ -6,12 +6,12 @@ export function Header() {
     <nav className="sticky top-0 z-50 flex justify-between mb-12 px-2 md:px-4 py-4 bg-background/50 backdrop-blur-sm border-b border-border">
       <Link
         href="/"
-        className="font-bold md:text-2xl px-4 py-2 text-primary hover:text-primary/80"
+        className="font-bold md:text-2xl py-2 text-primary hover:text-primary/80"
       >
         WILLIAMS
       </Link>
       <LightDarkToggle />
-      <div className="flex items-center gap-8 uppercase">
+      <div className="flex items-center gap-4 uppercase">
         <Link
           href="/projects"
           className="text-[14px] md:text-[16px] hover:text-primary"
