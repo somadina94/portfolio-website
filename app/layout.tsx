b5a759cac71d20436f22dbd96ec5545c4378936c
somadina94 <williamsonuaguluchi94@gmail.com>
2025-09-09 15:27:19 +0100

    Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import { ThemeProvider } from "@/context/theme-provider";
 import { Header } from "@/components/general/header";
@@ -37,6 +38,19 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-[1440px] mx-auto`}
       >
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-FDJQ4QESCB"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-FDJQ4QESCB');
+          `}
+        </Script>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
