3c89e82ee402c5930194c3a90851c51635466f1e
somadina94 <williamsonuaguluchi94@gmail.com>
2026-01-09 00:44:05 +0100

    Feature/Animation_Completion

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,6 +3,8 @@ import { createClient } from "@/lib/supabase/server";
 import Image from "next/image";
 import Link from "next/link";
 import { GlobeIcon, GithubIcon } from "lucide-react";
+import * as motion from "motion/react-client";
+import { container, item } from "@/utils/motion-config";
 
 export const metadata = {
   title: "Projects | Williams Onuaguluchi",
@@ -28,35 +30,49 @@ export default async function Projects() {
       </div>
       <div className="flex flex-col gap-12">
         {projects?.map((project) => (
-          <div
-            className="flex flex-col md:flex-row justify-between gap-12"
+          <motion.div
             key={project.id}
+            variants={container}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: false, amount: 0.25 }}
+            className="flex flex-col md:flex-row justify-between gap-12"
           >
-            <Card className="w-full flex justify-center items-center md:h-[400px]">
-              <CardContent>
-                <Image
-                  src={project.imageurl as string}
-                  alt="Featured Project"
-                  width={500}
-                  height={1000}
-                  className="rounded-lg"
-                />
-              </CardContent>
-            </Card>
-            <div className="flex flex-col gap-4 w-full">
+            {/* Image */}
+            <motion.div variants={item} className="w-full">
+              <Card className="w-full flex justify-center items-center md:h-[400px]">
+                <CardContent>
+                  <Image
+                    src={project.imageurl as string}
+                    alt="Featured Project"
+                    width={500}
+                    height={1000}
+                    className="rounded-lg"
+                  />
+                </CardContent>
+              </Card>
+            </motion.div>
+
+            {/* Content */}
+            <motion.div variants={item} className="flex flex-col gap-4 w-full">
               <h3>{project.title}</h3>
+
               <p className="text-muted-foreground">{project.description}</p>
+
               <span className="border-b text-lg uppercase pb-4">
                 Project Info
               </span>
+
               <div className="flex justify-between border-b border-border pb-4">
                 <span>Year</span>
                 <span>{project.year}</span>
               </div>
+
               <div className="flex justify-between border-b border-border pb-4">
                 <span>Role</span>
                 <span>Full Stack Developer</span>
               </div>
+
               <div className="flex gap-12">
                 <Link
                   href={project.demolink as string}
@@ -65,6 +81,7 @@ export default async function Projects() {
                   LIVE DEMO
                   <GlobeIcon />
                 </Link>
+
                 <Link
                   href={project.githublink as string}
                   className="flex gap-2 text-primary hover:text-primary/80 border-b border-primary"
@@ -73,8 +90,8 @@ export default async function Projects() {
                   <GithubIcon />
                 </Link>
               </div>
-            </div>
-          </div>
+            </motion.div>
+          </motion.div>
         ))}
       </div>
     </div>
